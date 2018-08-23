9cb0d8699b4685924fe484ed89c39cdece8dc574
vahyno <vahyno@gmail.com>
2018-08-23 16:40:39 -0700

    feat: installed react-router-dom, implemented routing

---
+++ src/components/BookList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+    render(){
+        return (
+        <div>
+            BookList
+        </div>)
+    }
+}
+
+export default BookList;
---
+++ src/components/SearchBooks.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class SearchBooks extends Component {
+    render(){
+        return(
+            <div>
+                SearchBooks
+            </div>
+        )
+    }
+}
+
+export default SearchBooks;
