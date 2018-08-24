665b2157261acc4160c07b7ce60f645894e674c7
vahyno <vahyno@gmail.com>
2018-08-23 21:57:09 -0700

    refactor: updated App.js with state, added componentDidMount, passing down state down

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import BookList from './components/BookList';
 import SearchBooks from './components/SearchBooks';
 import './App.css';
 
 class BooksApp extends Component {
+  state = {
+    books: [],
+  }
+
+  componentDidMount(){
+    BooksAPI.getAll()
+      .then(books => {
+        // console.log(books);
+        this.setState(() => ({
+          books,
+        }))
+      })  
+  }
+/**
+* @description Represents a book
+* @param {string} title - The title of the book
+* @param {string} author - The author of the book
+*/  
   
   render() {
+    console.log(this.state.books)
     return (
       <div className="app">
         <Route exact path='/' render={() => (
-            <BookList />
+            <BookList
+              books={this.state.books}
+            />
           )}
         />
         <Route path='/search' render={() => (
--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
+import Book from './Book';
 
 class Shelf extends Component {
     render(){
         return (
-        <div>
-            Shelf
-        </div>)
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{this.props.shelfName}</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        <Book />
+                    </ol>
+                </div>
+            </div>
+        )
     }
 }
 
