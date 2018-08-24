66c35a61d6d04f5cb5d4795b756bf00b92ab80d4
vahyno <vahyno@gmail.com>
2018-08-23 20:12:01 -0700

    feat: created 4 components (Book, BookList, SearchBooks and Shelf), Linked BookList and SearchBooks componets

---
+++ src/components/Shelf.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Shelf extends Component {
+    render(){
+        return (
+        <div>
+            Shelf
+        </div>)
+    }
+}
+
+export default Shelf;
